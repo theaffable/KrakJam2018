2088d49a398359e9809a44a2e8a0ba4459728ae9
kubabuba <kuba.szecowka@gmail.com>
2018-01-27 17:54:56 +0100

    Lights.

---
+++ Assets/Scripts/Utils/RotateLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+
+	private float _offset;
+	
+	void Start()
+	{
+		_offset = Random.Range(-Mathf.PI, Mathf.PI);
+	}
+	
+	void Update () {
+			transform.Rotate(0f, Mathf.Cos(Time.time + _offset), 0f, Space.Self);
+	}
+}
