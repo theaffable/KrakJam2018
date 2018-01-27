793c21a5780611a41196346abce82826e0f8ba67
Tiffussini Cannelloni <tif@krkjum.de>
2018-01-27 19:42:42 +0100

    [NT] Player fix.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
 	private int player = 1;
+
+	[SerializeField]
+	private Camera _camera;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,19 +20,21 @@ public class PlayerController : MonoBehaviour {
 		
 	// Update is called once per frame
 	void Update () {
-		if (player == 1) {
-			AV = Input.GetAxis ("Vertical");
-			AH = -Input.GetAxis ("Horizontal");
-		}
-		if (player == 2) {
-			AV = Input.GetAxis ("VerticalR");
-			AH = -Input.GetAxis ("HorizontalR");
-		}
-		//first make sure stick vals are higher than the tolerance
-		if ((Mathf.Abs (AH) > stickTol || Mathf.Abs (AV) > stickTol)) {
-			if ((Mathf.Abs (AH) + Mathf.Abs (AV)) > 1) {
-				float angle = Mathf.Atan2 (AH, AV) * Mathf.Rad2Deg;
-				transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+		if (_camera.isActiveAndEnabled) {
+			if (player == 1) {
+				AV = Input.GetAxis ("Vertical");
+				AH = -Input.GetAxis ("Horizontal");
+			}
+			if (player == 2) {
+				AV = Input.GetAxis ("VerticalR");
+				AH = -Input.GetAxis ("HorizontalR");
+			}
+			//first make sure stick vals are higher than the tolerance
+			if ((Mathf.Abs (AH) > stickTol || Mathf.Abs (AV) > stickTol)) {
+				if ((Mathf.Abs (AH) + Mathf.Abs (AV)) > 1) {
+					float angle = Mathf.Atan2 (AH, AV) * Mathf.Rad2Deg;
+					transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+				}
 			}
 		}
 	}
