ccf45edc163b2cd6e68d7e9e96d2ed81118b3191
kubabuba <kuba.szecowka@gmail.com>
2018-01-27 23:05:30 +0100

    Merged with master.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -23,17 +23,17 @@ public class PlayerController : MonoBehaviour {
 		if (_camera.isActiveAndEnabled) {
 			if (player == 1) {
 				AV = Input.GetAxis ("Vertical");
-				AH = Input.GetAxis ("Horizontal");
+				AH = -Input.GetAxis ("Horizontal");
 			}
 			if (player == 2) {
 				AV = Input.GetAxis ("VerticalR");
-				AH = Input.GetAxis ("HorizontalR");
+				AH = -Input.GetAxis ("HorizontalR");
 			}
 			//first make sure stick vals are higher than the tolerance
 			if ((Mathf.Abs (AH) > stickTol || Mathf.Abs (AV) > stickTol)) {
 				if ((Mathf.Abs (AH) + Mathf.Abs (AV)) > 1) {
 					float angle = Mathf.Atan2 (AH, AV) * Mathf.Rad2Deg;
-					transform.rotation = Quaternion.Euler (new Vector3 (0, 180f, angle));
+					transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
 				}
 			}
 		}
