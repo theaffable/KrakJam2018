240b4271aa759c08f9be7329a9472b13f63e28d5
czotomo <beltowski.t@gmail.com>
2018-01-28 04:00:01 +0100

    spawnable spawnables

---
+++ Assets/Scripts/Utils/DestroyAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public float ObjectTtl;
+
+    private float _spawnTime;
+
+    private void Start()
+    {
+        _spawnTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time >= _spawnTime + ObjectTtl)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
